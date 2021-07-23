c22b4cdb89ded5ff8171f164bf7ea42c06bdc9db
lavanyatammana24 <87838872+lavanyatammana24@users.noreply.github.com>
2021-07-22 18:39:09 -0700

    Update BeforeAfterMethodPgm.java
    second commit

--- ExtendReportProject/src/test/java/testNGPkg/BeforeAfterMethodPgm.java
+++ ExtendReportProject/src/test/java/testNGPkg/BeforeAfterMethodPgm.java
@@ -8,11 +8,11 @@ public class BeforeAfterMethodPgm
 	@BeforeMethod
 	public void beforeMethod()
 	{
-		System.out.println("Before Method");
+		System.out.println("Before Method updated in git ");
 	}
 	@AfterMethod
 	public void afterMethod()
 	{
-		System.out.println("After Method");
+		System.out.println("After Method updated in git");
 	}
 }
